9513ee2213841b0600d53dc087d1fc4864badfdc
fabien hollebeque <hollebeque.fabien@hotmail.com>
2021-08-24 18:30:12 +0200

    DefaultController

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ *
+ * @package App\Controller
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * Page index
+     *
+     * @return Response
+     *
+     * @Route("/", name="default")
+     */
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
