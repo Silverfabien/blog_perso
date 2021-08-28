536fc74dc576338ba3c4b6e188e598013b411080
fabien hollebeque <hollebeque.fabien@hotmail.com>
2021-08-28 18:57:33 +0200

    Ajout de la déconnexion de forcesi le compte est bloqué

---
+++ src/EventListener/UserBlockedListener.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Repository\User\BlockedRepository;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * Class UserBlockedListener
+ */
+class UserBlockedListener
+{
+    private $blockedRepository;
+    private $security;
+
+    /**
+     * @param BlockedRepository $blockedRepository
+     * @param Security $security
+     */
+    public function __construct(
+        BlockedRepository $blockedRepository,
+        Security $security
+    )
+    {
+        $this->blockedRepository = $blockedRepository;
+        $this->security = $security;
+    }
+
+    /**
+     * @param RequestEvent $event
+     */
+    public function __invoke(
+        RequestEvent $event
+    ): void
+    {
+        $currentUser = $this->security->getUser();
+
+        if($currentUser !== null) {
+            $userBlocked = $this->blockedRepository->findOneByUser($currentUser);
+
+            if ($userBlocked !== null) {
+                $this->security->getToken()->setAuthenticated(false);
+                throw new CustomUserMessageAuthenticationException('security.account.disabled');
+            }
+        }
+    }
+}
