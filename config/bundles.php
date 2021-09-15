3c8e26b0a195a1e96564f5f8d52f487618f8ee3e
fabien hollebeque <hollebeque.fabien@hotmail.com>
2021-09-15 20:33:59 +0200

    Ajout de ckeditor + config des toolbars

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
