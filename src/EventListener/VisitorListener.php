2e91e2e269f928ac3b0215de5fe42440451af089
fabien hollebeque <hollebeque.fabien@hotmail.com>
2021-10-17 17:58:13 +0200

    Ajout de la désactivation et la réactivation du profiler pour éviter un bug en dev

--- src/EventListener/VisitorListener.php
+++ src/EventListener/VisitorListener.php
@@ -8,6 +8,7 @@ use App\EventListener\Handler\VisitorHandler;
 use App\Repository\Visitor\VisitorRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
 use Symfony\Component\Security\Core\Security;
 
 class VisitorListener
@@ -16,18 +17,21 @@ class VisitorListener
     private $security;
     private $request;
     private $visitorHandler;
+    private $profiler;
 
     public function __construct(
         VisitorRepository $visitorRepository,
         Security $security,
         Request $request,
-        VisitorHandler $visitorHandler
+        VisitorHandler $visitorHandler,
+        Profiler $profiler
     )
     {
         $this->visitorRepository = $visitorRepository;
         $this->security = $security;
         $this->request = $request;
         $this->visitorHandler = $visitorHandler;
+        $this->profiler = $profiler;
     }
 
     public function onKernelRequest(
@@ -39,6 +43,8 @@ class VisitorListener
         $ip = $_SERVER['REMOTE_ADDR'];
         $visitorUserExist = $this->visitorRepository->findOneByUser($user);
         $visitorIpExist = $this->visitorRepository->findOneByIp($ip);
+
+        $this->profiler->disable();
 
         if ($user && !$visitorUserExist) {
             $visitor = new Visitor();
@@ -56,13 +62,14 @@ class VisitorListener
             return;
         }
 
-
         if (!$visitorIpExist) {
             $visitor = new Visitor();
             if ($this->visitorHandler->updateIfIpNotExist($visitor, $event)) {
                 return;
             }
         }
+
+        $this->profiler->enable();
 
         return;
     }
