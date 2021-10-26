65c29d3e90b0a8f4b3120ef785309002cb4eb6e8
fabien hollebeque <hollebeque.fabien@hotmail.com>
2021-10-26 18:20:11 +0200

    Ajout de la validation des cookies

---
+++ assets/styles/js/components/cookies.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function() {
+    window.wpcc.init({
+        "colors": {
+            "popup": {"background": "#222222", "text": "#ffffff", "border": "#fde296"},
+            "button": {"background": "#fde296", "text": "#000000"}
+        },
+        "transparency": "10", "content": {
+            "href": "/mentions_legales#cookies",
+            "message": "Blog - Hollebeque Fabien respecte votre vie privée. Nous n'utilisons aucun cookies à des fins de marketing ou de ciblage de masse. Seuls des cookies obligatoires, nécessaires ainsi qu'à des fins d'analyses et de statistiques sont présents. En continuant à utiliser et à naviguer sur notre site, vous acceptez et consentez à notre utilisation des cookies.",
+            "button": "J'ai compris",
+            "link": "En savoir plus"
+        },
+        "corners": "normal", "border": "normal", "position": "bottom", "margin":"small"
+    });
+});
