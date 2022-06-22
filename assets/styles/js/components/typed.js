0a95b17464f60cf828a0cfe63297447a1caf19be
fabien hollebeque <hollebeque.fabien@hotmail.com>
2022-06-22 02:22:45 +0200

    final commit

--- assets/styles/js/components/typed.js
+++ assets/styles/js/components/typed.js
@@ -1,7 +1,7 @@
 import Typed from "typed.js";
 
 var typed = new Typed(".typed", {
-    strings: ["Hollebeque Fabien", "Developpeur BackEnd sous Php/Symfony", "Hollebeque Fabien, Développeur Symfony !", "Hollebeque Fabien, Développeur Symfony 3, 4 et 5 !"],
+    strings: ["Nom Prénom", "Developpeur BackEnd sous Php/Symfony", "Nom Prénom, Développeur Symfony !", "Nom Prénom, Développeur Symfony 3, 4 et 5 !"],
     typeSpeed: 60,
     backDelay: 2000,
     backSpeed: 30,
